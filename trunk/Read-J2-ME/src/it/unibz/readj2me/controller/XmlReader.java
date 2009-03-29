dd155d994f714afad98c8ac5fce5b53635d097aa
omoling <omoling@769f054e-192f-11de-9308-ab3afd9c0aba>
2009-03-29 20:30:56 +0000

    xml parser to read news entries

--- trunk/Read-J2-ME/src/it/unibz/readj2me/controller/XmlReader.java
+++ trunk/Read-J2-ME/src/it/unibz/readj2me/controller/XmlReader.java
@@ -1,15 +1,24 @@
 package it.unibz.readj2me.controller;
 
+import it.unibz.readj2me.model.NewsItem;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Vector;
 import org.kxml2.io.*;
 import org.xmlpull.v1.*;
 
 public class XmlReader {
 
-    private final String XML_NAME = "/sampleatom.xml";
-    private final String ENCODING = "utf-8";
+    private final static String XML_NAME = "/sampleatom.xml";
+    private final static String ENCODING = "utf-8";
+    private final static String TAG_ENTRY = "entry";
+    private final static String TAG_ID = "id";
+    private final static String TAG_TITLE = "title";
+    private final static String TAG_PUBLISHED = "published";
+    private final static String TAG_UPDATED = "updated";
+    private final static String TAG_LINK = "link";
+    private final static String TAG_CONTENT = "content";
 
     private InputStream getXMLInputStream() {
         return this.getClass().getResourceAsStream(XML_NAME);
@@ -30,10 +39,12 @@ public class XmlReader {
                     int event = xmlParser.next();
 
                     if (event == XmlPullParser.END_DOCUMENT) {
-
-                        System.out.println("XML read!");
-
                         break;
+                    }
+                    if (event == XmlPullParser.START_TAG && xmlParser.getName().equals(TAG_ENTRY)) {
+                        NewsItem item;
+                        item = populateItem(xmlParser);
+                        entries.addElement(item);
                     }
 
                 }
@@ -46,5 +57,38 @@ public class XmlReader {
         }
 
         return entries;
+    }
+
+    private NewsItem populateItem(KXmlParser xmlParser) throws XmlPullParserException, IOException {
+        NewsItem item = new NewsItem();
+
+        while (true) {
+            int event = xmlParser.next();
+            
+            if (event == XmlPullParser.END_DOCUMENT ||
+                    (event == XmlPullParser.END_TAG && xmlParser.getName().equals(TAG_ENTRY))) {
+                break;
+            }
+
+            if (event == XmlPullParser.START_TAG) {
+                if (xmlParser.getName().equals(TAG_ID)) {
+                    String id = xmlParser.nextText();
+                    item.setId(Integer.parseInt(id));
+                } else if (xmlParser.getName().equals(TAG_TITLE)) {
+                    String title = xmlParser.nextText();
+                    item.setTitle(title);
+                } /*else if(xmlParser.getName().equals(TAG_PUBLISHED)){
+                   *
+                }*/ else if(xmlParser.getName().equals(TAG_LINK)){
+                item.setLink(xmlParser.getAttributeValue(null, "href"));
+                } else if(xmlParser.getName().equals(TAG_CONTENT)){
+                    String content = xmlParser.nextText();
+                    item.setContent(content);
+                }
+            }
+
+        }
+
+        return item;
     }
 }
