385680c35c9166b8a389ea1862503300485b1770
omoling <omoling@769f054e-192f-11de-9308-ab3afd9c0aba>
2009-04-14 21:45:13 +0000

    some refactoring, constants-class created, itemView extended

--- trunk/Read-J2-ME/src/it/unibz/readj2me/model/NewsItem.java
+++ trunk/Read-J2-ME/src/it/unibz/readj2me/model/NewsItem.java
@@ -12,6 +12,55 @@ public class NewsItem {
     private String content = "";
     private String summary = "";
 
+    public NewsItem(){
+    }
+
+    public NewsItem(byte[] recordData){
+        createFromBytes(recordData);
+    }
+
+    public byte[] getBytes(){
+        StringBuffer sb = new StringBuffer();
+        sb.append(id);
+        sb.append(Constants.FIELD_SEPARATOR);
+        sb.append(title);
+        sb.append(Constants.FIELD_SEPARATOR);
+        sb.append(content);
+        sb.append(Constants.FIELD_SEPARATOR);
+        sb.append(summary);
+        sb.append(Constants.FIELD_SEPARATOR);
+        sb.append(link);
+        /* TODO
+        sb.append(FIELD_SEPARATOR);
+        sb.append(published.toString());
+        sb.append(FIELD_SEPARATOR);
+        sb.append(updated.toString());
+        */
+        return sb.toString().getBytes();
+    }
+
+    private void createFromBytes(byte[] recordData){
+        String recordString = new String(recordData);
+        int index1, index2;
+        index1 = recordString.indexOf(Constants.FIELD_SEPARATOR);
+        id = recordString.substring(0, index1);
+        index2 = recordString.indexOf(Constants.FIELD_SEPARATOR, index1 + 1);
+        title = recordString.substring(index1 + 1, index2);
+        index1 = index2;
+        index2 = recordString.indexOf(Constants.FIELD_SEPARATOR, index1 + 1);
+        content = recordString.substring(index1 + 1, index2);
+        index1 = index2;
+        index2 = recordString.indexOf(Constants.FIELD_SEPARATOR, index1 + 1);
+        summary = recordString.substring(index1 + 1, index2);
+        index1 = index2;
+
+        //TODO: when dates are implemented, update string.length to index2
+        //index2 = recordString.indexOf(FIELD_SEPARATOR, index1 + 1);
+        //link = recordString.substring(index1 + 1, index2);
+        link = recordString.substring(index1 + 1, recordString.length());
+        //TODO do the same for published and updated!!
+    }
+
     /**
      * @return the title
      */
